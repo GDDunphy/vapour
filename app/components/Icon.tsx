aeb39a75b53de65f46afada40c8583108d81c293
hughdunphy <hughdunphy@hotmail.co.uk>
2024-04-15 13:17:31 +0100

    lint

--- app/components/Icon.tsx
+++ app/components/Icon.tsx
@@ -1,7 +1,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faSquareCheck } from '@fortawesome/free-solid-svg-icons/faSquareCheck'
-import { faMugSaucer } from '@fortawesome/free-solid-svg-icons/faMugSaucer'
+import {faMugSaucer} from '@fortawesome/free-solid-svg-icons/faMugSaucer';
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons/faArrowRight";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 library.add(fab, faSquareCheck, faMugSaucer, faArrowRight)
